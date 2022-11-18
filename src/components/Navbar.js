a6495315041e82ceaf247c0a674c46b52bfd0cec
Scott H <zenwattage@gmail.com>
2022-11-17 16:25:41 -0800

    adding cart plus & minus buttons, navbar item count function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import { Button, Container, Navbar, Modal } from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { CartContext } from '../CartContext';
 
 function NavbarComponent() {
+
+    const cart = useContext(CartContext);
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+
+    const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
 
     return (
         <>
@@ -13,7 +19,7 @@ function NavbarComponent() {
                 <Navbar.Brand href="/"> E-Commerce </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Button variant="dark" onClick={handleShow}> Cart 0 Items</Button>
+                    <Button variant="dark" onClick={handleShow}> Cart ({productsCount}) Items</Button>
                 </Navbar.Collapse>
             </Navbar>
             <Modal show={show} onHide={handleClose}>
